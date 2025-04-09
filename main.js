e15de1ffe0399faf5788ad71ab2c2e29ba065476
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-04-08 22:20:53 -0300

    criação e implementação da função responsável por colorir o dia com a cor selecionada no HTML (colorirDia() no main.js)

---
+++ main.js
@@ -0,0 +1,48 @@
+function colorirDia() {
+  //Pegarvalor do campo de dia
+  let days = document.getElementById('days').value;
+
+  //Pega valor do campo de cor
+  let color = document.getElementById('color').value;
+
+  //Pega os elementos da tabela calendario
+  let calendar = document.getElementById('calendar');
+
+  //verifica se o campo de dia foi preenchido
+  if (!days) {
+    alert('Favor informar o dia');
+  } else if ((days > 0) && (days < 31)) {
+    //Se o numero de duas está no intevalo aplicará a cor na célula do calendário
+    let td = calendar.getElementsByTagName('td')[parseInt(days) + 1];
+    td.style.backgroundColor = color;
+  } else {
+    alert('Favor informar um dia válido');
+  }
+
+  //Guarda a célula da tabela
+  let elementos = document.querySelectorAll('td');
+
+  //Declara e inicializa contadores
+  let contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+
+  //Loop para verificar o estilo de cada célula e contar quantas tem cor e qual cor
+
+  for (let i = 0; i < elementos.length; i++) {
+    //obtém o estilo computado do elemento atual
+    var estilo = window.getComputedStyle(elementos[i]);
+
+    //Obtém a cor de fundo atual da célula
+    var corEstilo = estilo.backgroundColor;
+
+    //Verifica a cor e incrementa o contador correspondente
+    if ((corEstilo === 'rgb(173, 216, 230') || (corEstilo === 'lightblue')) { // azul
+      contadorAzul++;
+    } else if (corEstilo === 'rgb(152, 215, 152)' || (corEstilo === 'palegreen')) { // verde
+      contadorVerde++;
+    } else if (corEstilo === 'rgb(255, 192, 203)' || (corEstilo === 'lightpink')) { // rosa
+      contadorRosa++;
+    } else if (corEstilo === 'rgb(255, 182, 193)' || (corEstilo === 'slateblue')) { // roxo
+      contadorRoxo++;
+    }
+  }
+}
